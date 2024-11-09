fdcf60fa40703a56e9096e77cd19ce1d8fb96faf
martinbarnas <barnas.martin@gmail.com>
2024-11-09 17:33:57 +0800

    try out a form

--- app/(tabs)/two.tsx
+++ app/(tabs)/two.tsx
@@ -1,13 +1,33 @@
-import {Button, Text, View} from 'tamagui'
+import {Button, Checkbox, Form, H2, Input, Label, Text, View} from 'tamagui'
+import {Check, CheckCircle} from "@tamagui/lucide-icons";
 
 export default function TabTwoScreen() {
+  const onSubmitHandler = () => {
+    console.log(`Form submitted`);
+  };
+
   return (
-    <View flex={1} alignItems="center" justifyContent="center" bg="$background">
-      <Text fontSize={20} color="$blue10">
-        Tab Two
-      </Text>
+    <View bg="$background" px={'$2'}>
+      <H2 color="$blue10">Tab Two</H2>
+
+      <Form onSubmit={onSubmitHandler}>
+        <Label htmlFor={'name'} px={'$4'}>Name</Label>
+        <Input id="name" defaultValue={'0'}/>
+
+        <Checkbox size={'$5'} my={'$4'}>
+          <Checkbox.Indicator>
+            <Check/>
+          </Checkbox.Indicator>
+        </Checkbox>
+
+        <Form.Trigger asChild>
+          <Button icon={CheckCircle}>
+            Submit
+          </Button>
+        </Form.Trigger>
+      </Form>
+
       <Button mt={'$5'} size={'$6'}>Hello World!</Button>
-      <Button mt={'$5'} size={'$5'}>Hello World !</Button>
     </View>
   )
 }
